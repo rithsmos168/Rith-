75a7d6c44115457e036b6d6441d783ef2e3d1e1e
rithsmos168 <rithamm72@gmail.com>
2026-02-11 08:49:23 +0700

    new code

---
+++ untitled13.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+	string name;
+	string a,b,c,d,e;
+	cout << "Enter your full name : "cin >> name;
+	cout << "Enter C++ Programing Score : " cin>> a;
+	cout << "Enter Computer repair Score : " cin>>b;
+	return 0;
+	
+	
+}
+
